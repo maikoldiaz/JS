caf12691778d3c53d0c208f7a99c0e769540dc99
Maikol Diaz <diazhoyamaikol@gmail.com>
2019-09-16 16:58:52 -0500

    node js

---
+++ jsnode/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const logger = require("morgan");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+require("./routes/views")(app);
+require("./routes/special")(app);
+require("./routes/api")(app);
+
+console.log("Iniciando Express.js");
+app.listen(3000, ()=>{
+    console.log("Express ha iniciado correctamente!");
+});
