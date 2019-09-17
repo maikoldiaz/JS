27a9ab621fc56ecd028043a2238f73244af8a119
Maikol Diaz <diazhoyamaikol@gmail.com>
2019-09-17 16:01:09 -0500

    nodejs primera clase con mongodb

--- jsnode/index.js
+++ jsnode/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const bodyParser = require("body-parser");
+const mongo = require("./db/connect");
 const app = express();
 
 app.use(logger("dev"));
@@ -10,7 +11,19 @@ require("./routes/views")(app);
 require("./routes/special")(app);
 require("./routes/api")(app);
 
-console.log("Iniciando Express.js");
-app.listen(3000, ()=>{
-    console.log("Express ha iniciado correctamente!");
-});
+async function InitMongo(){
+    const db = await mongo.connect();
+    if (db){initExpress();}
+}
+function initExpress(){
+    console.log("Iniciando Express");
+    app.listen(3000, ()=>{
+        console.log("Express ha iniciado correctamente!");
+        process.on("SIGINT", closeApp);
+        process.on("SIGTERM", closeApp);   
+    });
+}
+function closeApp(){
+    mongo.disconnect().
+            then(()=>process.exit(0));
+}
