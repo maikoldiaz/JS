06762504031a2daa4d9a872830319e58ddd4670b
Maikol Diaz <diazhoyamaikol@gmail.com>
2019-11-08 16:56:34 -0500

    add firt version answer

--- platzi-overflow/src/app/answer/answer-form.component.ts
+++ platzi-overflow/src/app/answer/answer-form.component.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Answer, User } from './answer.model';
+import { Question } from '../question/question.model';
 
 @Component({
     selector: 'app-answer-form',
-    templateUrl: './answer-form.component.html'
+    templateUrl: './answer-form.component.html',
+    styles: [`
+    form{
+        display: flex;
+        flex-direction:column;
+        width : 90%;
+        margin:40px auto;
+        margin-top : 20px;
+    }
+    `]
 })
 
 export class AnswerFormComponent {
+    @Input() question: Question;
     onSubmit(form: NgForm) { //metodo con el nombre que comunemtante se utiliza para mandar datos.
-        console.log(form.value.description);
-
+        const answer = new Answer(
+            form.value.description,
+            this.question,
+            new Date(),
+            new User("Maikol","Diaz")
+        );
+        this.question.answers.unshift(answer);
+        form.reset();
     }
 }
